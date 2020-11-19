7b23d5fbd18a2adcd0c5d5827c0e74a8d6a37120
Courtney <Courtney@Kais-iMac.fritz.box>
2020-11-19 11:51:06 +0100

    updated preprocess script names

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # preprocessing steps
-python src/preprocess_reference.py data/NIST2000 data SWBD
-python src/preprocess_reference.py data/NIST2000 data CH
-python src/preprocess_human.py data/human_parity data True
+python src/preprocess_ref.py data/NIST2000 data SWBD
+python src/preprocess_ref.py data/NIST2000 data CH
+python src/preprocess_hyp.py data/human_parity data
