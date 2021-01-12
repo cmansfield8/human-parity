6d10cb09e60213fab479df4d63b2fcd90ba56aa7
Courtney Mansfield <cmansfield8@gmail.com>
2021-01-12 12:17:46 +0100

    added sclite output option setting

--- src/wer.sh
+++ src/wer.sh
@@ -4,9 +4,10 @@ DATADIR=$1
 OUT=$2
 
 SCLITE="/home2/coman8/SCTK/bin/sclite"
+OUTPUT="sgml"
 
-$SCLITE -h ${DATADIR}/human.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o all -O $OUT
-$SCLITE -h ${DATADIR}/machine.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o all -O $OUT
+$SCLITE -h ${DATADIR}/human.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o $OUTPUT -O $OUT
+$SCLITE -h ${DATADIR}/machine.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o $OUTPUT -O $OUT
 
-$SCLITE -h ${DATADIR}/human.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o all -O $OUT
-$SCLITE -h ${DATADIR}/machine.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o all -O $OUT
+$SCLITE -h ${DATADIR}/human.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o $OUTPUT -O $OUT
+$SCLITE -h ${DATADIR}/machine.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o $OUTPUT -O $OUT
