b24f95b287e01eb0f4f5cd9d623c97d16e16515f
Courtney Mansfield <cmansfield8@gmail.com>
2021-01-22 17:03:19 +0100

    fix output

--- src/wer.sh
+++ src/wer.sh
@@ -5,14 +5,14 @@ OUT=$2
 
 SCLITE="/home2/coman8/SCTK/bin/sclite"
 
-$SCLITE -h ${DATADIR}/human.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o $OUTPUT -O all
-$SCLITE -h ${DATADIR}/human.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o $OUTPUT -O sgml
+$SCLITE -h ${DATADIR}/human.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o all
+$SCLITE -h ${DATADIR}/human.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o sgml
 
-$SCLITE -h ${DATADIR}/machine.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o $OUTPUT -O all
-$SCLITE -h ${DATADIR}/machine.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o $OUTPUT -O sgml
+$SCLITE -h ${DATADIR}/machine.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o all
+$SCLITE -h ${DATADIR}/machine.swb_processed.ctm ctm -r ${DATADIR}/SWBDO.stm stm -F -o sgml
 
-$SCLITE -h ${DATADIR}/human.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o $OUTPUT -O all
-$SCLITE -h ${DATADIR}/human.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o $OUTPUT -O sgml
+$SCLITE -h ${DATADIR}/human.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o all
+$SCLITE -h ${DATADIR}/human.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o sgml
 
-$SCLITE -h ${DATADIR}/machine.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o $OUTPUT -O all
-$SCLITE -h ${DATADIR}/machine.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o $OUTPUT -O sgml
+$SCLITE -h ${DATADIR}/machine.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o all
+$SCLITE -h ${DATADIR}/machine.callhome_processed.ctm ctm -r ${DATADIR}/CHO.stm stm -F -o sgml
